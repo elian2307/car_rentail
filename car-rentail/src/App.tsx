4c22d2e1086aafe7a7706d82bc0c59e1844100e9
Elian <105528688+elian2307@users.noreply.github.com>
2026-04-29 12:00:31 -0600

    se agrego el Dashboard

--- car-rentail/src/App.tsx
+++ car-rentail/src/App.tsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
-import Card from "./components/Card"
+import Dashboard from './views/Dashboard'
+import './App.css'
 
 function App() {
-  const [elemento, setElemento] = useState([]) as any
-  const [nombre, setNombre] = useState("");
-  const aumentar = () => {
-    setElemento([...elemento, nombre]);
-
-
-  }
 
   return (
     <>
-    <h1>{nombre}</h1>
-    <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-    <button onClick={aumentar}>Aumentar</button>
-    {
-      elemento.map((item: string, index: number) => (
-        <Card key={index} />
-      ))
-    }
+      <Dashboard />
     </>
   )
 }
